81a44ad6182be571bfe2d384b128c99e8239ed6e
Gabriel Mascarenhas <devgabrielprog@gmail.com>
2026-07-10 06:31:51 -0300

    Projeto para Empresa AnaMaria

---
+++ script.js
@@ -0,0 +1,149 @@
+const menu = document.getElementById("menu")
+const cartBtn = document.getElementById("cart-btn")
+const cartModal = document.getElementById("cart-modal")
+const cartItemsContainer = document.getElementById("cart-items")
+const cartTotal = document.getElementById("cart-total")
+const cartCounter = document.getElementById("cart-count")
+const address = document.getElementById("address")
+const addressWarn = document.getElementById("address-warn")
+const closeModalBtn = document.getElementById("close-modal-btn")
+const checkoutBtn = document.getElementById("checkout-btn")
+
+let cart = [];
+
+
+// Abrir modal do carrinho
+cartBtn.addEventListener("click", function(){
+    updateCartModal();
+    cartModal.style.display = "flex"
+})
+
+//Fechar o modal ao clicar fora
+cartModal.addEventListener("click", function(event){
+    if(event.target === cartModal){
+        cartModal.style.display = "none"
+    }
+})
+
+closeModalBtn.addEventListener("click", function(){
+    cartModal.style.display = "none"
+})
+
+//Pegar as propriedados dos itens no menu
+menu.addEventListener("click", function(event){
+    let parentBtn = event.target.closest(".add-to-cart-btn") 
+
+    if(parentBtn){
+        const name = parentBtn.getAttribute("data-name")
+        const price = parseFloat(parentBtn.getAttribute("data-price"))
+        //Adicionar no carrinho
+        addToCart(name, price)
+    }
+})
+
+//Função para adicionar ao carrinho
+function addToCart(name, price){
+    const existingItem = cart.find(item => item.name === name)
+
+    if(existingItem){
+       existingItem.quantity += 1; 
+    }else{
+        cart.push({
+        name,
+        price,
+        quantity: 1, 
+    })
+    }
+    updateCartModal()
+}
+
+//Atualiza o carrinho
+function updateCartModal(){
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item => {
+        const cartItemElement = document.createElement("div");
+        cartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col")
+
+        cartItemElement.innerHTML = `
+          <div class="flex items-center justify-between">
+              <div>
+                 <p class="font-medium">${item.name}</p>
+                 <p>Qtd: ${item.quantity}</p>
+                 <p class="font-medium mt-2">R$ ${item.price.toFixed(2)}</p>
+              </div>
+              <button class="text-red-500 hover:font-medium remove-from-cart-btn" data-name="${item.name}">
+                 Remover
+              </button>         
+            </div>
+        `
+        total += item.price * item.quantity;
+        cartItemsContainer.appendChild(cartItemElement)
+    })
+    cartTotal.textContent = total.toLocaleString("pt-BR",
+        {style: "currency",
+         currency: "BRL"   
+        });
+
+    cartCounter.innerHTML = cart.length;
+}
+
+//Função para remover o item do carrinho
+cartItemsContainer.addEventListener("click", function(event){
+    if(event.target.classList.contains("remove-from-cart-btn")){
+        const name = event.target.getAttribute("data-name")
+
+        removeItemCart(name)
+    }
+})
+
+function removeItemCart(name){
+    const index = cart.findIndex(item => item.name == name)
+
+    if(index !== -1){
+        const item = cart[index]
+
+        if(item.quantity > 1){
+            item.quantity -= 1;
+            updateCartModal();
+            return;
+        }
+
+        cart.splice(index, 1)
+        updateCartModal()
+    }
+}
+
+address.addEventListener("input", function(event){
+    let inputValue = event.target.value;
+
+    if(inputValue !== ""){
+        console.log(inputValue)
+        address.classList.remove("border-red-500")
+        addressWarn.classList.add("hidden")
+    }
+})
+
+checkoutBtn.addEventListener("click", function(){
+    if(cart.length === 0) return;
+    if(address.value === ""){
+        addressWarn.classList.remove("hidden")
+        address.classList.add("border-red-500")
+        return;
+    }
+    //Enviar o pedido para API WhatsApp 
+        const cartItems = cart.map((item) => {
+            return (
+                `${item.name} Quantidade: (${item.quantity}) Preço: R$${item.price} |`
+            )
+        }).join("")
+
+        const message = encodeURIComponent(cartItems)
+        const phone = "86999253632"
+
+        window.open(`https://wa.me/${phone}?text=${message} Endereço: ${address.value}`, "_blank")
+
+        cart.length = 0;
+        updateCartModal();
+})
